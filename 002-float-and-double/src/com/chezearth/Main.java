c02521c10ebdbfa97c7d28a318782cc6923563f2
Xiaolei Bi <bixiaolei@gmail.com>
2022-08-02 12:21:05 +0200

    c

--- 002-float-and-double/src/com/chezearth/Main.java
+++ 002-float-and-double/src/com/chezearth/Main.java
@@ -9,20 +9,21 @@ public class Main {
         // width of int is 32 (4 bytes)
         int myIntValue = 5 / 2;
         // width of float is 32 (4 bytes)
-        myFloatValue = (float)5 / 3;//myFloatValue = 5f / 3f;
+        myFloatValue = (float)5 / 3;//prefer to use myFloatValue = 5f / 3f;
         // width of double is 64 (8 bytes)
         double myDoubleValue = 5d / 3d;
 
-        System.out.println("myIntValue = " + myIntValue);
-        
+        System.out.println("myIntValue = " + myIntValue); //sout is shortcut for System.out.println 
+ 
         System.out.println("myFloatValue = " + myFloatValue);
+    
         System.out.println("myDoubleValue = " + myDoubleValue);
 
 
         double lbValue = 200d;
         double kgValue = lbValue * 0.453_592_37d;
         System.out.println(lbValue + " lb converts to " + kgValue + " kg");
-        double myKgWeight = 99.6d;
+        double myKgWeight = 99.6d; //double is the default type for variables
         double myLbWeight = myKgWeight / 0.453_592_37d;
         System.out.println("My body weight in lbs is " + myLbWeight);
 
